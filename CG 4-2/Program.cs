09066a5fc13cf186eeca77a131ca7849efaca257
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-08-02 22:50:38 -0500

    Change the variable type to int

--- CG 4-2/Program.cs
+++ CG 4-2/Program.cs
@@ -6,13 +6,23 @@ namespace CG_4_2
     {
         static void Main(string[] args)
         {
-            string [] value = new string [] { 1, 1, 2, 3, 5, 8 };
-            foreach (string values in value)
+            /*So this creates our array on one line.  Initally I used var but the program
+            got angry at me and I don't know why. I tried changing the variable type within
+            the foreach statement but it didn't like that.  I tried having the variable types all
+            be string but then I either had to convert the numbers to a string to get it to read
+            or I would have to put each number in quotations. Which just seemed like a lot of 
+            unnecessary work.  So I made it all the variable types
+            to be int, which seems to have appeased the beast.*/
+              
+         
+            int [] value = new int[] { 1, 1, 2, 3, 5, 8 };
+            foreach (int values in value)
             {
                 Console.WriteLine(values);
             }
 
-            Console.ReadLine();
+            Console.Readline();
+
         }
     }
 }
